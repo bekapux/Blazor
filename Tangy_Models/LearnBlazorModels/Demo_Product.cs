a8e475ba60f5943ee98ed2f59ed11393fb051cb9
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-05-20 23:03:22 +0400

    solved 24. assignment 2 - property binding

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -3,9 +3,9 @@
   public class Demo_Product
   {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public double Price { get; set; }
     public bool IsActive { get; set; }
-    public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+    public List<Demo_ProductProp> ProductProperties { get; set; }
   }
 }
