df5e9cc58a779c017c6733262e98f6ba5f4fdf62
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-05-24 18:23:43 +0400

    learned js

---
+++ Tangy_server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace Tangy_server.Helper
+{
+  public static class IJSRuntimeExtension
+  {
+    public static async ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string Message)
+    {
+      await jSRuntime.InvokeVoidAsync("ShowToastr", "success", Message);
+    }
+    public static async ValueTask ToastrError(this IJSRuntime jSRuntime, string Message)
+    {
+      await jSRuntime.InvokeVoidAsync("ShowToastr", "error", Message);
+    }
+  }
+}
---
+++ Tangy_server/wwwroot/js/common.js
@@ -0,0 +1,30 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Operation Successful', { timeOut: 5000 })
+    }
+    else if (type === "warning") {
+        toastr.warning(message)
+    }
+    else if (type === "error") {
+        toastr.error(message, 'Operation Failed!', { timeOut: 5000 })
+    }
+    else if (type === "clear") {
+        toastr.clear()
+    }
+    else if (type === "remove") {
+        toastr.remove()
+    }
+    else if (type === "warning") {
+        toastr.warning(message)
+    }
+}
+
+window.ShowSweetAlert = (title, message, icon, button) => {
+    swal(
+        {
+            title: title,
+            text: message,
+            icon: icon,
+            button: button,
+        });
+}
